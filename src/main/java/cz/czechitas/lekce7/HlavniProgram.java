ce7e8770a85a0f01b0663502177068417a8b03ae
Michala Lišková <michala.liskova@bluedynamic.cz>
2025-06-01 23:34:02 +0200

    Odevzdání úkolu z č. 5 z lekce 7 po otestování "SvatkyTest"

--- src/main/java/cz/czechitas/lekce7/HlavniProgram.java
+++ src/main/java/cz/czechitas/lekce7/HlavniProgram.java
@@ -1,7 +1,9 @@
 package cz.czechitas.lekce7;
 
+import javax.swing.*;
 import java.time.Month;
 import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Hlaví třída pro lekci 7.
@@ -15,15 +17,40 @@ public class HlavniProgram {
      */
     public static void main(String... args) {
         Svatky svatky = new Svatky();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.");
 
-        System.out.printf("V seznamu je %d jmen.", svatky.getPocetJmen()).println();
-        System.out.printf("Monika má svátek %s.", svatky.vratKdyMaSvatek("Monika")).println();
+        //    System.out.printf("V seznamu je %d jmen.", svatky.getPocetJmen()).println();
+        // svatky.smazSvatek("Monika");
+        String jmeno = "Monika";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno)).println();
+        jmeno = "Eva";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno)).println();
+
         System.out.printf("Eva je v seznamu svátků: %b.", svatky.jeVSeznamu("Eva")).println();
+        System.out.printf("Eva je v seznamu svátků: %s.", svatky.jeVSeznamuToString("Eva")).println();
+        svatky.smazSvatek("Monika");
+        svatky.pridejSvatek("Sofie", 15, 5);
+        svatky.pridejSvatek("Štěpán", 26, Month.DECEMBER);
+
+        jmeno = "Sofie";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno)).println();
+
+        jmeno = "Štěpán";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno)).println();
+        svatky.pridejSvatek("Pinda", 6, 4);
+        jmeno = "Pinda";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno).format(formatter)).println();
+        svatky.pridejSvatek("Fifinka", 16, 4);
+        jmeno = "Fifinka";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno).format(formatter)).println();
+        jmeno = "Sofie";
+        System.out.printf("%s má svátek %s.", jmeno, svatky.vratKdyMaSvatek(jmeno)).println();
+
 
         System.out.println("V seznamu jsou následující jména:");
-        svatky.getSeznamJmen().forEach(jmeno -> System.out.printf("- %s", jmeno).println());
+        svatky.getSeznamJmen().forEach(jmena -> System.out.printf("- %s", jmena).println());
 
-/*
+
         svatky.smazSvatek("Přemysl");
         System.out.printf("Po smazání Přemysla je v seznamu je %d jmen.", svatky.getPocetJmen()).println();
 
@@ -32,7 +59,7 @@ public class HlavniProgram {
         svatky.pridejSvatek("Tamara", MonthDay.of(6, 3));
         svatky.pridejSvatek("Kevin", MonthDay.of(Month.JUNE, 3));
         System.out.printf("Po přidání Laury, Jarmila, Tamary a Kevina je v seznamu je %d jmen.", svatky.getPocetJmen()).println();
-*/
+
     }
 
 }
